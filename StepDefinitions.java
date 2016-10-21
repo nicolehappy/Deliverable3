58766dc9653fd96a776924c03e08891814dbe238
nicolehappy <nicholeli4129@gmail.com>
2016-10-20 20:46:25 -0400

    fix configuration issues

--- StepDefinitions.java
+++ StepDefinitions.java
@@ -19,19 +19,22 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class StepDefinitions {
-    private WebDriver driver;
-    private WebDriverWait wait;
-    
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+
     private final String HOME_PAGE = "http://store.demoqa.com/";
 
     /*
      * homepage feature
+     * As a user, I am very first to this website
+     * I will check the title of the website
      */
 
     @Given("a Firefox browser")
@@ -40,49 +43,52 @@ public class StepDefinitions {
         driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, 30);
     }
-    
+
     @When("I navigate to the home page")
     public void navigateHome() {
         driver.get(HOME_PAGE);
     }
-    
+
     @Then("the title should be (.*)")
     public void checkPageTitle(String title) {
         assertEquals(title, driver.getTitle());
     }
 
-    @Given("a the product page")
-    public void openproduct(){
-        System.setProperty("webdriver.gecko.driver", "libs/geckodriver");
-        driver = new FirefoxDriver();
-        wait = new WebDriverWait(driver, 30);
-        driver.get("http://store.demoqa.com/products-page/product-category/");
-    }
-    @When("I click the logo picture at the head of the page")
+
+    @When("I click the logo picture")
     public void clickLogo(){
         WebElement logo = driver.findElement(By.id("logo"));
         logo.click();
     }
-    @Then("I should be back to the home page")
+    @Then("I should be stay in home page")
     public void checkLogo(){
         wait = new WebDriverWait(driver, 15);
         assertEquals(HOME_PAGE, driver.getTitle());
     }
 
 
+
+
+
+
     /*
      * login feature
+     * As a user, I always need to login to this website
+     * If I have typed right username and password, i should be able to login
+     * Also there are some cases that I might fail to login
      */
-    @Given("^a login page$")
-    public void LoginPage(){
+
+    @Given("a login page")
+    public void LoginPage() {
         System.setProperty("webdriver.gecko.driver", "libs/geckodriver");
-        driver = new FirefoxDriver();
-        wait = new WebDriverWait(driver, 30);
+        //driver = new FirefoxDriver();
+        //wait = new WebDriverWait(driver, 30);
         driver.get("http://store.demoqa.com/products-page/your-account/");
     }
 
+
     @When("I login with both username and password correct")
-    public void loginSuccess(){
+    public void loginSuccess() {
         WebElement username = driver.findElement(By.id("log"));
         WebElement password = driver.findElement(By.id("pwd"));
         username.sendKeys("yul134");
@@ -90,13 +96,13 @@ public class StepDefinitions {
         WebElement login = driver.findElement(By.id("login"));
         login.click();
     }
-    @Then("I should be able to logout")
-    public void logout(){
-            wait = new WebDriverWait(driver, 15);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("(Logout)")));
-            driver.findElement(By.linkText("(Logout)"));
-        }
 
+    @Then("I should be able to logout")
+    public void logout() {
+        wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("(Logout)")));
+        driver.findElement(By.linkText("(Logout)"));
+    }
 
     @When("I login with wrong username or password")
     public void loginFail1() {
@@ -107,22 +113,25 @@ public class StepDefinitions {
         WebElement login = driver.findElement(By.id("login"));
         login.click();
     }
-    @Then("I should see error message")
-    public void errorMessage1(){
+
+    @Then("I should see an error message")
+    public void errorMessage1() {
         wait = new WebDriverWait(driver, 15);
         WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p.response")));
         assertEquals("ERROR: Invalid login credentials.", message.getText());
     }
 
+
     @When("I login with empty username")
-    public void loginFail2(){
+    public void loginFail2() {
         WebElement username = driver.findElement(By.id("log"));
         username.clear();
         WebElement login = driver.findElement(By.id("login"));
         login.submit();
     }
-    @Then("I should see error message")
-    public void errorMessage2(){
+
+    @Then("I should see a notice message")
+    public void errorMessage2() {
         wait = new WebDriverWait(driver, 15);
         WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p.response")));
         assertEquals("Please enter your username and password.", message.getText());
@@ -130,79 +139,41 @@ public class StepDefinitions {
 
     /*
      * shopping cart feature
+     * As a e-commercial website, shopping cart is quite a vital feature
+     * As a user, I will add my preferred product to the shopping cart,
+     * and I will update the shopping cart such as remove items
+     * or update the quantity of the item
      */
 
-    @Given("that I am on the product category page")
-    public void openProduct(){
+    @Given("the product category page")
+    public void openProduct() {
         System.setProperty("webdriver.gecko.driver", "libs/geckodriver");
-        driver = new FirefoxDriver();
-        wait = new WebDriverWait(driver, 30);
         driver.get("http://store.demoqa.com/products-page/product-category/");
     }
 
-    @When("I choose to buy an iPhone5")
-    public void buyProduct(){
-        WebElement buy = driver.findElement(By.name("Buy"));
-        buy.submit();
-        wait = new WebDriverWait(driver, 15);
-
-    }
-    @Then("I should see a successful message pop out")
-    public void checkAdd(){
-        wait = new WebDriverWait(driver, 15);
-        WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p.response")));
-        assertEquals("You just added \"iPhone 5\" to your cart.", message.getText());
-    }
-
-    @When("After I add an iPhone5 to shopping cart, I click check out")
-    public void checkout(){
+    @When("I choose to buy an iPhone5 and click check out")
+    public void buyProduct() {
         WebElement buy = driver.findElement(By.name("Buy"));
         buy.submit();
         wait = new WebDriverWait(driver, 15);
         WebElement goToCart = wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Go to Checkout")));
         goToCart.click();
+
     }
-    @Then("I should see the iPhone is already in the shopping cart")
-    public void checkcart(){
+
+    @Then("I should have the right information in the shopping cart")
+    public void checkAdd() {
         wait = new WebDriverWait(driver, 15);
         WebElement cart = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.slide1")));
         String str = cart.getText();
         assertTrue(str.contains("iPhone 5"));
-
-    }
-    @Given("I am on the shopping cart page with one iPhone5 in it")
-    public void openCart1(){
-            System.setProperty("webdriver.gecko.driver", "libs/geckodriver");
-            driver = new FirefoxDriver();
-            wait = new WebDriverWait(driver, 15);
-            driver.get("http://store.demoqa.com/products-page/product-category/n/");
-            WebElement buy = driver.findElement(By.name("Buy"));
-            buy.submit();
-            WebElement goToCart = wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Go to Checkout")));
-            goToCart.click();
-    }
-    @When("I would like to buy 2 iPhone5")
-    public void updateCart(){
-        wait = new WebDriverWait(driver, 15);
-        WebElement quantity = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("quantity")));
-        quantity.clear();
-        quantity.sendKeys("2");
-        WebElement update = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("submit")));
-        update.click();
-    }
-    @Then("the shopping cart should be updated")
-    public void checkUpdate(){
-        wait = new WebDriverWait(driver, 15);
-        WebElement quantity = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("quantity")));
-        assertEquals("2",quantity.getAttribute("value"));
-        WebElement total = driver.findElement(By.cssSelector("td.wpsc_product_price.wpsc_product_price_0"));
-        assertEquals("$24.00",total.getText());
+        WebElement subtotal = driver.findElement(By.cssSelector("span.pricedisplay"));
+        assertEquals("$12.00", subtotal.getText());
     }
 
-    @Given("I am on the shopping cart page with one iPhone5 in it")
-    public void openCart2(){
+    @Given("the shopping cart page with one iPhone5 in it")
+    public void openCart1() {
         System.setProperty("webdriver.gecko.driver", "libs/geckodriver");
-        //System.setProperty("webdriver.gecko.driver", "libs\\geckodriver.exe");
         driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, 15);
         driver.get("http://store.demoqa.com/products-page/product-category/n/");
@@ -211,13 +182,33 @@ public class StepDefinitions {
         WebElement goToCart = wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Go to Checkout")));
         goToCart.click();
     }
+
+    @When("I change the quantity to 2 and click update")
+    public void updateCart() {
+        wait = new WebDriverWait(driver, 15);
+        WebElement quantity = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("quantity")));
+        quantity.clear();
+        quantity.sendKeys("2");
+        WebElement update = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("submit")));
+        update.click();
+    }
+
+    @Then("the shopping cart should be updated")
+    public void checkUpdate() {
+        wait = new WebDriverWait(driver, 15);
+        WebElement quantity = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("quantity")));
+        assertEquals("2", quantity.getAttribute("value"));
+
+    }
+
     @When("I click remove button")
-    public void removeItem(){
+    public void removeItem() {
         wait = new WebDriverWait(driver, 15);
         WebElement remove = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("form.adjustform.remove > input[name=\"submit\"]")));
         remove.click();
     }
-    @Then("the item should be removed from the shopping cart")
+
+    @Then("the iPhone should be removed from the shopping cart")
     public void checkRemovingItem() {
         wait = new WebDriverWait(driver, 15);
         if (wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("form.adjustform.remove > input[name=\"submit\"]")))) {
@@ -227,11 +218,64 @@ public class StepDefinitions {
         }
     }
 
+/*
+ * search feature
+ * as a user, i would like to search for items
+ * the website should be able to return right information
+ *
+ */
 
-    @After
-    public void cleanUp() {
-        driver.quit();
+    @Given("a search box")
+    public void findSearchBox() {
+        System.setProperty("webdriver.gecko.driver", "libs/geckodriver");
+        //System.setProperty("webdriver.gecko.driver", "libs\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 30);
+        driver.get(HOME_PAGE);
+    }
+    @When("I type iPhone 5")
+    public void searchMagicMouse() {
+        WebElement searchbox = driver.findElement(By.name("s"));
+        searchbox.clear();
+        searchbox.sendKeys("iPhone 5");
+        searchbox.sendKeys(Keys.RETURN);
     }
 
+    @Then("I should see the iPhone 5 listed in the result")
+    public void checkResult() {
+        wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h2.prodtitle")));
+        WebElement result = driver.findElement(By.cssSelector("div.product_grid_display.group"));
+        assertTrue(result.getText().contains("iPhone"));
+    }
 
-}
+    @When("I type iphone")
+    public void searchiphone() {
+        WebElement searchbox = driver.findElement(By.name("s"));
+        searchbox.clear();
+        searchbox.sendKeys("iphone");
+        searchbox.sendKeys(Keys.RETURN);
+    }
+
+    @When("I type keyboard")
+    public void searchkeyboard() {
+        WebElement searchbox = driver.findElement(By.name("s"));
+        searchbox.clear();
+        searchbox.sendKeys("Keyboard");
+        searchbox.sendKeys(Keys.RETURN);
+    }
+
+    @Then("It should be nothing matched")
+    public void searchMessage() throws InterruptedException {
+        wait = new WebDriverWait(driver, 15);
+        Thread.sleep(5000);
+        WebElement result = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("content")));
+        assertEquals("Sorry, but nothing matched your search criteria. Please try again with some different keywords.",result.getText());
+    }
+
+        @After
+        public void cleanUp() {
+            driver.quit();
+        }
+    }
+
